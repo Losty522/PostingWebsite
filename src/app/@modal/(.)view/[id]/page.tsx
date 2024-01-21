deede4248b1e32a32d49f3387e08764aa26bafa6
Kenshiro Yasu <ken5221993@gmail.com>
2024-01-20 23:26:36 -0800

    fixed some design

--- src/app/@modal/(.)view/[id]/page.tsx
+++ src/app/@modal/(.)view/[id]/page.tsx
@@ -12,9 +12,7 @@ const page = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="flex justify-center items-center bg-black bg-opacity-60 p-5 fixed top-0 left-0 h-full w-full">
-      {/* <div className="flex flex-col bg-white text-black p-3 h-1/2 w-1/2 sm:h-1/2 w-1/2"> */}
-      {/* <div className="flex flex-col bg-white text-black p-3 sm:h-3/4 w-3/4 md:h-3/4 w-1/2 lg:h-3/4 w-1/2 xl:h-3/4 w-1/2"> */}
-      <div className="flex flex-col bg-white text-black p-3 w-11/12 h-4/5 text-center">
+      <div className="flex flex-col bg-white text-black p-3 w-11/12 h-full text-center">
         <Link
           href=".."
           className="self-end  text-zinc-900 px-2 py-1 text-center hover:text-red-700 "
@@ -26,22 +24,24 @@ const page = async ({ params }: { params: { id: string } }) => {
         <p className="max-w-full w-full break-words text-xl">
           {noteItem?.content}
         </p>
-        <div className="mx-auto  w-3/4 h-3/4">
-          <img src={String(noteItem?.image)} alt="" className="" />
-        </div>
+        <img
+          src={String(noteItem?.image)}
+          alt=""
+          className="mx-auto  w-11/12 h-3/4 max-w-fit max-h-fit object-cover"
+        />
 
-        <div className="flex w-6/12 mx-auto mt- ">
+        <div className="flex w-2/4 mx-auto justify-between mt-5">
           <Link
             href={`/edit/${params.id}`}
             // onClick={handleEdit}
-            className="border  border-zinc-900 text-zinc-900 px-2 py-1 text-cente bg-blue-400 hover:bg-blue-300 hover:text-black "
+            className="border  border-zinc-900 text-zinc-900 px-2 py-1 text-center bg-blue-400 hover:bg-blue-300 hover:text-black "
           >
             Edit
           </Link>
 
           <Link
             href={`/delete/${params.id}`}
-            className="ml-auto border border-zinc-900 text-zinc-900 bg-red-400 px-2 py-1 text-center hover:bg-red-500
+            className="border border-zinc-900 text-zinc-900 bg-red-400 px-2 py-1 text-center hover:bg-red-500
            hover:text-black "
           >
             Delete
